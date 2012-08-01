2a6bc0676768b42e47895869838c517b1ec08f7e
Stephanie Shupe <shupe.stephanie@gmail.com>
2012-07-31 19:34:25 -0700

    Initial commit

---
+++ free_url/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeUrl::Application.config.secret_token = 'fcac33c0162cafab94c615c10ee344c0902659215a32a19d579b11384fc7fe17f6792614b21588a83ac67ea71be7c8c5261b10c20b0f586d32bce47b4f17bfb7'
