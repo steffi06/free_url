5ff466d3975eefcb3eba204003fef43d2ea795e4
Stephanie Shupe <shupe.stephanie@gmail.com>
2012-08-01 21:55:46 -0700

    Add uri validation

--- free_url/app/models/url.rb
+++ free_url/app/models/url.rb
@@ -1,3 +1,9 @@
 class Url < ActiveRecord::Base
   attr_accessible :long, :short
+  
+  VALID_URI = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)|(^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+  # VALID_URI_NO_HTTP = /(^$)|(^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+  
+  validates :long, presence: true, uri: { format: VALID_URI }
+  validates :short, :presence => true
 end
